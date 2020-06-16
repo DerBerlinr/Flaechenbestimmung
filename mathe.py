56867aaa996c49ea892928e72fbd5d5dc3ccdd6f
eri47 <34981166+eri47@users.noreply.github.com>
2020-06-16 08:25:04 +0200

    Add files via upload

--- mathe.py
+++ mathe.py
@@ -4,25 +4,25 @@ import math
 runden = input("Bitte die Anzahl der Runden -> ")
 intervall_start = input("Bitte den Startpunkt des Intervalls -> ")
 intervall_ende = input("Bitte den Endpunkt des Intervalls -> ")
-hoehe = float(intervall_ende)*math.e**-float(intervall_ende)
-print(hoehe)
-flaeche = (float(intervall_ende) - float(intervall_start)) * 1
+hoehe = 1/math.sqrt(1-float(intervall_ende)**2)
+flaeche = (float(intervall_ende) - float(intervall_start)) * hoehe
 treffer = 0
 
 for i in range(int(runden)):
     x = random.uniform(float(intervall_start), float(intervall_ende))
     #print(x)
-    y = random.uniform(0, 1)
+    y = random.uniform(0, hoehe)
     #print(y)
-    if y < -2*(x**2)+x:
+    if y < 1/math.sqrt(1-(x**2)):
         treffer += 1
         #print("Treffer!")
     else:
         pass
         #print("Nicht getroffen!")
 
-print("Treffer: ", treffer)
-print("Runden: ", runden)
-print("Fläche: ", flaeche)
+for i in range(10):
+    print()
+
+print("In", runden, "Runden wurden", treffer, "Treffer auf einer insgesamten Fläche von", round(flaeche, 2), "FE erzielt.")
 ergebnis = float(treffer) / float(runden) * flaeche
-print("Ergebnis = ", ergebnis)
+print("Somit ist die gesuchte Fläche", round(ergebnis, 2), "FE groß.")
